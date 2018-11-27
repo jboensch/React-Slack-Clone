b7a37fd0b7538b2e3f9333a15b627e4a43e83ee6
Joshua Boensch <josh.boensch@gmail.com>
2018-11-27 11:43:11 -0500

    Section 5 - Lesson 14
    Add Spinner Component for Loading State

--- src/index.js
+++ src/index.js
@@ -1,56 +1,70 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import Login from './components/Auth/Login';
-import Register from './components/Auth/Register';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import Login from "./components/Auth/Login";
+import Register from "./components/Auth/Register";
+import Spinner from "./Spinner";
+import registerServiceWorker from "./registerServiceWorker";
 import firebase from "./firebase";
 
-import 'semantic-ui-css/semantic.min.css';
+import "semantic-ui-css/semantic.min.css";
 
-import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    withRouter
+} from "react-router-dom";
 
-import { createStore } from 'redux';
-import { Provider, connect } from 'react-redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import rootReducer from './reducers';
-import { setUser } from './actions';
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
+import { setUser } from "./actions";
 
 const store = createStore(rootReducer, composeWithDevTools());
 
 class Root extends React.Component {
-    //if we did mount
     componentDidMount() {
         firebase.auth().onAuthStateChanged(user => {
-            //see if we have an authenticated user
             if (user) {
-                //console.log(user);
+                // console.log(user);
                 this.props.setUser(user);
-                //if so, redirect into root of site using withRouter
                 this.props.history.push("/");
             }
-        })
+        });
     }
+
     render() {
-        return (
-            <Switch>
-                <Route exact path="/" component={App} />
-                <Route path="/login" component={Login} />
-                <Route path="/register" component={Register} />
-            </Switch>
-        );
+        return this.props.isLoading ? (
+            <Spinner />
+        ) : (
+                <Switch>
+                    <Route exact path="/" component={App} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/register" component={Register} />
+                </Switch>
+            );
     }
 }
 
-//used to ensure we are authenticated
-const RootWithAuth = withRouter(connect(null, { setUser })(Root));
+const mapStateFromProps = state => ({
+    isLoading: state.user.isLoading
+});
 
-//to be able to access the global state we are creating in const store, we need to wrap our Router in Provider
+const RootWithAuth = withRouter(
+    connect(
+        mapStateFromProps,
+        { setUser }
+    )(Root)
+);
+
 ReactDOM.render(
     <Provider store={store}>
         <Router>
             <RootWithAuth />
         </Router>
-    </Provider>
-    , document.getElementById('root'));
+    </Provider>,
+    document.getElementById("root")
+);
 registerServiceWorker();
