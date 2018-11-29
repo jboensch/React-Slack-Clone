6bb5ddeebbe99c02ac72e908d2ebc0fa404463d5
Joshua Boensch <josh.boensch@gmail.com>
2018-11-29 16:47:23 -0500

    S8: 28, 29
    Create Messages Collection, Add First Message to Database, Controlled
    Input to Clear Message, Disabled Button on Loading State

--- src/components/Messages/MessagesHeader.js
+++ src/components/Messages/MessagesHeader.js
@@ -9,14 +9,19 @@ class MessagesHeader extends React.Component {
                 <Header fluid="true" as="h2" floated="left" style={{ marginBottom: 0 }}>
                     <span>
                         Channel
-                    <Icon name={"star outline"} color="black" />
+            <Icon name={"star outline"} color="black" />
                     </span>
                     <Header.Subheader>2 Users</Header.Subheader>
                 </Header>
 
                 {/* Channel Search Input */}
                 <Header floated="right">
-                    <Input size="mini" icon="search" name="searchTerm" placeholder="Search Messages" />
+                    <Input
+                        size="mini"
+                        icon="search"
+                        name="searchTerm"
+                        placeholder="Search Messages"
+                    />
                 </Header>
             </Segment>
         );
